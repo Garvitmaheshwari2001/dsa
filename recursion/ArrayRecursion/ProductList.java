a2af261b32d605c86f14125686a8f3b7d85f3c63
Garvitmaheshwari2001 <86588690+Garvitmaheshwari2001@users.noreply.github.com>
2023-06-12 17:19:49 +0530

    Add files via upload

---
+++ recursion/ArrayRecursion/ProductList.java
@@ -0,0 +1,20 @@
+package recursion.Day3;
+
+public class ProductList {
+
+    static int[] square(int []arr,int n)
+    {
+        if(n == arr.length)
+        {
+            return new int[n];
+        }
+        int curr[] = square(arr, n + 1);
+        curr[n] = arr[n] * arr[n];
+        return curr;
+    }
+    public static void main(String[] args) {
+        int[] res = square(new int[] { 1, 2, 3, 4, 5, 6 }, 0);
+        for(int x:res)
+        System.out.print(x + " ");
+    }
+}
---
+++ recursion/ArrayRecursion/UpperCase.java
@@ -0,0 +1,25 @@
+package recursion.Day3;
+
+public class UpperCase {
+
+    static void solve(String[] arr,String[] res,int n)
+    {
+        if(n== arr.length)
+        {
+            return;
+        }
+        String ans = arr[n];
+        ans = ans.toUpperCase();
+        res[n] = ans;
+        solve(arr, res, n+1);
+    }
+    public static void main(String[] args) {
+        String[] arr = { "foo", "bar", "world" };
+        String[] res = new String[arr.length];
+        solve(arr, res, 0);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(res[i] + " ");
+        }
+    }
+}
