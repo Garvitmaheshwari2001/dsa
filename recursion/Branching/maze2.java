1827e27e4c6dd4fc942fe5be757b46cce52ab9ea
Garvitmaheshwari2001 <86588690+Garvitmaheshwari2001@users.noreply.github.com>
2023-06-13 14:56:51 +0530

    Add files via upload

---
+++ recursion/Branching/maze2.java
@@ -0,0 +1,25 @@
+public class maze2 {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 3;
+        maze(0, 0, n, m, "");
+
+    }
+
+    public static void maze(int i, int j, int n, int m, String ls) {
+        if (i == n - 1 && j == m - 1) {
+            System.out.println(ls);
+            return;
+        }
+        if (i > n) {
+            return;
+        }
+        if (j > m) {
+            return;
+        }
+        maze(i + 1, j, n, m, ls + "R");
+        maze(i, j + 1, n, m, ls + "D");
+        maze(i + 1, j + 1, n, m, ls + "I");
+    }
+}
